8947b9af652e63a8ab6d6def2638db9d755cb6b6
Anthony Calderaio <anthonycalderaio@Anthonys-MacBook-Pro.local>
2020-06-13 17:14:46 -0400

    working level 1 concept

--- virtual-table-UI/src/app/app.component.ts
+++ virtual-table-UI/src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'virtual-table-UI';
+
+  proteinData = {
+    "0": {
+      "name": "protease2",
+      "image_path": "./assets/protease2_image.png"
+    },
+    "1": {
+      "name": "spike",
+      "image_path": "./assets/spike_image.png"
+    },
+    "2": {
+      "name": "protease",
+      "image_path": "./assets/protease_image.png"
+    },
+    "3": {
+      "name": "spike1",
+      "image_path": "./assets/spike1_image.png"
+    }
+  }
+
+  level1Click(item: any) {
+    console.log('Clicked level 1', item);
+  }
 }
