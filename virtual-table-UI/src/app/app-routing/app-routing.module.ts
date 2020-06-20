44f23028e42e9870a64bcf2c74ed8a0d8e1a2741
Anthony Calderaio <anthonycalderaio@Anthonys-MacBook-Pro.local>
2020-06-20 16:23:36 -0400

    fixed pathing

--- virtual-table-UI/src/app/app-routing/app-routing.module.ts
+++ virtual-table-UI/src/app/app-routing/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
-import { SecondLevelComponent } from '/Users/anthonycalderaio/Desktop/Code/virtual-flow_projects/virtual-flow_virtual_table/virtual-table-UI/src/app/second-level/second-level.component';
-import { AppComponent } from '/Users/anthonycalderaio/Desktop/Code/virtual-flow_projects/virtual-flow_virtual_table/virtual-table-UI/src/app/app.component';
-import { FirstLevelComponent } from '/Users/anthonycalderaio/Desktop/Code/virtual-flow_projects/virtual-flow_virtual_table/virtual-table-UI/src/app/first-level/first-level.component';
+import { SecondLevelComponent } from '../second-level/second-level.component';
+import { AppComponent } from '../app.component';
+import { FirstLevelComponent } from '../first-level/first-level.component';
 import { ThirdLevelComponent } from '../third-level/third-level.component';
 
 
