2f2071cca86637a2ec281fe530b9ab36021a642b
Anthony Calderaio <anthonycalderaio@Anthonys-MacBook-Pro.local>
2020-06-20 18:32:48 -0400

    testing ngl and works!

--- virtual-table-UI/src/app/first-level/first-level.component.ts
+++ virtual-table-UI/src/app/first-level/first-level.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import testData from '../testData.json'
+import testData from '../testData.json';
+import * as NGL from '../../../node_modules/ngl';
 
 @Component({
   selector: 'app-first-level',
@@ -15,15 +16,27 @@ export class FirstLevelComponent implements OnInit {
 
   ngOnInit(): void {
     console.log('WholeDataJson: ', this.testData);
+    
+      var stage = new NGL.Stage("viewport");
+
+      // Handle window resizing
+      window.addEventListener("resize", function (event) {
+        stage.handleResize();
+      }, false);
+
+
+      // Load PDB entry 1CRN
+      stage.loadFile("rcsb://1crn", { defaultRepresentation: true });
+    
+
     // console.log('proteinData: ',this.proteinData)
   }
   testData: any = testData
 
 
 
-
   level1Click(item: any) {
-    
+
     // console.log('Clicked level 1', item);
     // Object.keys(this.proteinData).forEach(item => {
     //   console.log()
