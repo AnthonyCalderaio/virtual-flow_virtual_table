c1042b89ecee3d81eba12e037ab1950c22c71562
Anthony Calderaio <anthonycalderaio@Anthonys-MacBook-Pro.local>
2020-07-01 23:39:22 -0400

    works

--- virtual-table-UI/src/app/third-level/third-level.component.ts
+++ virtual-table-UI/src/app/third-level/third-level.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
 import * as molstar from 'src/assets/outsider_projects/version1/molstar.js';
@@ -12,8 +12,10 @@ declare  var jQuery:  any;
 export class ThirdLevelComponent implements OnInit {
   constructor(
     private _location: Location,
-    private router: Router) {
+    private router: Router,
+    private el: ElementRef) {
     }
+    HTMLElement: any;
 
   ngOnInit(): void {
     // console.log('thirdLevelData(thirdLevel): ', this.thirdLevelData);
@@ -33,10 +35,19 @@ export class ThirdLevelComponent implements OnInit {
   });
 
   viewer.loadStructuresFromUrlsAndMerge([
-      { url: pdbqtUrl, format: 'pdbqt' },
-      { url: mol2Url, format: 'mol2' }
+      { url: './assets/sample_urls/Receptor.pdbqt', format: 'pdbqt' },
+      { url: './assets/sample_urls/Ligand.mol2', format: 'mol2' }
   ]);
+  this.HTMLElement = document.getElementsByClassName("msp-plugin")[0];
 
+  // this.HTMLElement = this.el.nativeElement(`msp-plugin`);
+  // this.HTMLElement.setAttribute("style", "max-width: 80%;")
+  this.HTMLElement.setAttribute("style", "top: 60%; margin:5% ")
+
+  //Below one is top
+  // this.HTMLElement.setAttribute("style", "top: 60%; max-width:80%; left:50%")
+  // this.HTMLElement.setAttribute("style", "bottom: 10%;")
+  console.log('computer style:'+this.HTMLElement)
     
   }
 
