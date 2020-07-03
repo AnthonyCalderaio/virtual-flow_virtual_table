bb2b6a8b9aa5ba5ad7c95d8daebaf76f478de092
Anthony Calderaio <anthonycalderaio@Anthonys-MacBook-Pro.local>
2020-07-02 21:06:53 -0400

    cleanup molstart on third level

--- virtual-table-UI/src/app/third-level/third-level.component.ts
+++ virtual-table-UI/src/app/third-level/third-level.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
 import * as molstar from 'src/assets/outsider_projects/version1/molstar.js';
-declare  var jQuery:  any;
+declare var jQuery: any;
 
 @Component({
   selector: 'app-third-level',
@@ -14,8 +14,8 @@ export class ThirdLevelComponent implements OnInit {
     private _location: Location,
     private router: Router,
     private el: ElementRef) {
-    }
-    HTMLElement: any;
+  }
+  HTMLElement: any;
 
   ngOnInit(): void {
     // console.log('thirdLevelData(thirdLevel): ', this.thirdLevelData);
@@ -32,23 +32,18 @@ export class ThirdLevelComponent implements OnInit {
       viewportShowSettings: false,
       viewportShowSelectionMode: false,
       viewportShowAnimation: false,
-  });
+    });
 
-  viewer.loadStructuresFromUrlsAndMerge([
+    viewer.loadStructuresFromUrlsAndMerge([
       { url: './assets/sample_urls/Receptor.pdbqt', format: 'pdbqt' },
       { url: './assets/sample_urls/Ligand.mol2', format: 'mol2' }
-  ]);
-  this.HTMLElement = document.getElementsByClassName("msp-plugin")[0];
+    ]);
+    this.HTMLElement = document.getElementsByClassName("msp-plugin")[0];
+    var elementWidth = this.HTMLElement.getBoundingClientRect().width
+    console.log('elementWidth:', elementWidth);
+    this.HTMLElement.setAttribute("style", "top: 32%; margin:5%;")
+    console.log('computer style:' + this.HTMLElement)
 
-  // this.HTMLElement = this.el.nativeElement(`msp-plugin`);
-  // this.HTMLElement.setAttribute("style", "max-width: 80%;")
-  this.HTMLElement.setAttribute("style", "top: 60%; margin:5% ")
-
-  //Below one is top
-  // this.HTMLElement.setAttribute("style", "top: 60%; max-width:80%; left:50%")
-  // this.HTMLElement.setAttribute("style", "bottom: 10%;")
-  console.log('computer style:'+this.HTMLElement)
-    
   }
 
   wholeData = JSON.parse(JSON.stringify(this.router.getCurrentNavigation().extras))
