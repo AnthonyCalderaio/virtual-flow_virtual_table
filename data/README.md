dc9738d153be42d76ccce66f4b2460df19a210db
mgechev <mgechev@gmail.com>
2020-07-19 21:42:14 +0300

    Generate protein data and associate compounds and colors
    This PR adds:
    1. Raw data for the proteins, compounds, and colors.
    2. A script for their generation.

---
+++ data/README.md
@@ -0,0 +1,6 @@
+# Raw to JSON
+
+1. Convert `proteins.csv` from `raw-data` to JSON and save it to the current directory as `proteins.json`. For the CSV to JSON transform you can use https://csvjson.com/csv2json.
+2. Convert `colors.csv` from `raw-data` to JSON and save it to the current directory as `colors.json`. For the CSV to JSON transform you can use https://csvjson.com/csv2json.
+3. Run `index.js` to get the output `data.json` file.
+
---
+++ data/index.js
@@ -0,0 +1,61 @@
+const { writeFileSync } = require('fs');
+const { join } = require('path');
+const csv = require('csvtojson');
+const { dotCase } = require('dot-case');
+
+Promise.all([
+  csv().fromFile(join('raw-data', 'proteins.csv')),
+  csv().fromFile(join('raw-data', 'colors.csv')),
+])
+  .then(async ([proteins, colors]) => {
+    proteins = proteins.map(normalizeObject);
+    const colorMap = toMap(colors, 'inter_screen_id');
+    proteins.forEach((p) => {
+      p.color = colorMap[p.inter_screen_id].color;
+    });
+
+    for (const protein of proteins) {
+      await readCompounds(protein);
+    }
+
+    writeFileSync('data.json', JSON.stringify(toMap(proteins, 'inter_screen_id'), null, 2));
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+const readCompounds = async protein => {
+  const data = (
+    await csv().fromFile(join('raw-data', protein.inter_screen_id, 'data.csv'))
+  ).map(normalizeObject);
+  protein.compounds = toMap(data, 'compound_screening_id');
+};
+
+const toMap = (arr, key) => {
+  return arr.reduce((accum, current) => {
+    accum[current[key]] = current;
+    return accum;
+  }, {});
+};
+
+const normalizeObject = (o) => {
+  const result = {};
+  Object.keys(o).forEach((c) => {
+    const normalized = normalizeName(c);
+    result[normalized] = o[c];
+  });
+  return result;
+};
+
+const normalizeName = (name) => {
+  const first = name[0].toLowerCase();
+  const rest = name.slice(1);
+  return snakeCase(first + rest);
+};
+
+const snakeCase = (input, options) => {
+  return dotCase(input, {
+    delimiter: '_',
+    ...options,
+  });
+};
